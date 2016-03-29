d856c9e34b8d00ac66c1bd516d4612c241ca4251
Steve Morton <stevemorton@steves-mbp.apollosrc.root>
2016-03-29 15:23:24 -0400

    Initial commit

---
+++ project/gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+var htmlhint = require('gulp-htmlhint');
+var css_validation = require('gulp-css-validator');
+var jslint = require('gulp-jslint');
+var browserify = require('gulp-browserify');
+var ugligy = require('gulp-uglify');
+
+gulp.task('default', ['html', 'js', 'css']);
+
+gulp.task('html', function () {
+   return gulp.src('index.html')
+//        .pipe(htmlhint())
+//        .pipe(htmlhint.failReporter())
+        .pipe(gulp.dest('./public/'));
+});
+
+gulp.task('css', function() {
+   return gulp.src('./css/*.css')
+//        .pipe(css_validation())
+        .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('js', function () {
+    return gulp.src('./js/app.js')
+//        .pipe(jslint({
+//            browser: true,
+//            sloppy: true,
+//            node: true,
+//        }))
+        .pipe(browserify())
+        //.pipe(uglify())
+        .pipe(gulp.dest('./public/js'));
+});
