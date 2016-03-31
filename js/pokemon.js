4ac2991fe4d1c5ceca9ab2146671e630d6351da9
Michael <mamorton1092@gmail.com>
2016-03-31 14:21:01 -0400

    pokemon

---
+++ js/pokemon.js
@@ -0,0 +1,20 @@
+module.exports = [{
+    id: 0,
+    name: 'PikaChu',
+    type: 'Electric',
+}, 
+{
+    id: 1,
+    name: 'Charizard',
+    type: 'Fire',
+}, 
+{
+    id: 2,
+    name: 'Mewtwo',
+    type: 'Psychic',
+}, 
+{
+    id: 3,
+    name: 'Blastoise',
+    type: 'water',
+}];
